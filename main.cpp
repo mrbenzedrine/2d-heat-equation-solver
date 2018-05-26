057ad4ee97a3f8b81c1b68a6fcc18dcc1399c5da
Yousef Moazzam <mrbenzedrine@live.co.uk>
2018-05-26 20:27:11 +0100

    Define the constant matrices in the method

--- main.cpp
+++ main.cpp
@@ -67,4 +67,51 @@ int main()
         V(V.rows() - 1, j) = U(U.rows() - 1, j) = y_upper_dirichlet_bc_func(xSpacePoints(j, 0));
     }
 
+    // Define submatrices that will form the block matrices in the method equation
+
+    Eigen::internal::BandMatrix<double> D_x(N_x - 1, N_x - 1, 1, 1);
+    Eigen::internal::BandMatrix<double> D_y(N_y - 1, N_y - 1, 1, 1);
+    Eigen::MatrixXd I_x = Eigen::MatrixXd::Identity(N_x - 1, N_x - 1);
+    Eigen::MatrixXd I_y = Eigen::MatrixXd::Identity(N_y - 1, N_y - 1);
+
+    for(int i = -D_x.subs(); i <= D_x.supers(); i++)
+    {
+        D_x.diagonal(i).setConstant(0);
+        D_y.diagonal(i).setConstant(0);
+    }
+
+    // Define some useful constants
+
+    double q = dt/pow(dx, 2.0);
+    double r = dt/pow(dy, 2.0);
+
+    D_x.diagonal(0).setConstant(-2.0 * q);
+    D_x.diagonal(-1).setConstant(1.0 * q);
+    D_x.diagonal(1).setConstant(1.0 * q);
+
+    D_y.diagonal(0).setConstant(-2.0 * r);
+    D_y.diagonal(-1).setConstant(1.0 * r);
+    D_y.diagonal(1).setConstant(1.0 * r);
+
+    // Define Kronecker product matrices
+
+    Eigen::MatrixXd kroneckerProdMatrixA(I_y.rows() * D_x.rows(), I_y.cols() * D_x.cols());
+    Eigen::MatrixXd kroneckerProdMatrixB(D_y.rows() * I_x.rows(), D_y.cols() * I_x.cols());
+
+    for(int i = 0; i < I_y.rows(); i++)
+    {
+        for(int j = 0; j < I_y.cols(); j++)
+        {
+            kroneckerProdMatrixA.block(i * D_x.rows(), j * D_x.cols(), D_x.rows(), D_x.cols()) = I_y(i, j) * D_x.toDenseMatrix();
+        }
+    }
+
+    for(int i = 0; i < D_y.rows(); i++)
+    {
+        for(int j = 0; j < D_y.cols(); j++)
+        {
+            kroneckerProdMatrixB.block(i * I_x.rows(), j * I_x.cols(), I_x.rows(), I_x.cols()) = D_y.toDenseMatrix()(i, j) * I_x;
+        }
+    }
+
 }
